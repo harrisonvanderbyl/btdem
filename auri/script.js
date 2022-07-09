5aea0fb2eded08533f72d73128cd7d7bbb1c87a5
harrisonvanderbyl <harrison.vanderbyl@gmail.com>
2022-07-09 12:30:21 +1000

    add auri site

---
+++ auri/script.js
@@ -0,0 +1,146 @@
+const data = {
+  author: {
+    name: "Auri",
+    penName: "Brrt",
+    bio: "Subject of the Biography Beneath The Dragon Eye Moons.",
+    description:
+      "Join Auri as she travels through the world of Pallos, with magic, monsters, and a global system everyone can tap into. ",
+    image:
+      "https://cdn.discordapp.com/attachments/981003578422468658/995147382687797368/6.jpeg",
+  },
+  books: [
+    {
+      link: "https://www.amazon.com/gp/product/B08NWJMXXV",
+      image: "https://m.media-amazon.com/images/I/41KLgX1qPIL.jpg",
+    },
+    {
+      link: "https://www.amazon.com/gp/product/B08QMPDDKL",
+      image: "https://m.media-amazon.com/images/I/41sDoV-2AZL._SY300_.jpg",
+    },
+    {
+      link: "https://www.amazon.com/gp/product/B08Y5DSDQF",
+      image: "https://m.media-amazon.com/images/I/51R5hmfEWWL._SY300_.jpg",
+    },
+    {
+      link: "https://www.amazon.com/gp/product/B094YTHBML",
+      image: "https://m.media-amazon.com/images/I/51L1Nhhl0eL._SY300_.jpg",
+    },
+    {
+      link: "https://www.amazon.com/gp/product/B09GTTY8G8",
+      image: "https://m.media-amazon.com/images/I/417Uv0pZfRL._SY300_.jpg",
+    },
+    {
+      link: "https://www.amazon.com/gp/product/B09Q2SS1BT",
+      image: "https://m.media-amazon.com/images/I/510Wxp+OzjL._SY300_.jpg",
+    },
+    {
+      link: "https://www.amazon.com/gp/product/B09ZPQBW2C",
+      image: "https://m.media-amazon.com/images/I/51lwRCeLUtL._SY300_.jpg",
+    },
+  ],
+  artwork: [
+    "https://cdn.discordapp.com/attachments/981003578422468658/995147382687797368/6.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995147382448726066/5.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995147382171893820/4.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995147381790232606/3.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995147381433696357/2.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995147380993314906/1.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995147237476794368/6.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995147235992010803/3.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995147235790704703/2.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995146313886875698/6.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995146313467437076/4.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995146313240952912/3.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995146312750202940/1.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995143627435167864/6.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995143627082834040/5.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995143626550165544/3.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995143626294304898/2.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/995143626076213389/1.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/994785303028043886/5.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/994785301983674398/2.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/994785301497131140/1.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/994783335438102579/6.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/994783334964142090/5.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/994783334620201060/4.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/994783334083350619/2.jpeg",
+    "https://cdn.discordapp.com/attachments/981003578422468658/994783333831684186/1.jpeg",
+    "https://media.discordapp.net/attachments/765332529812078612/986491166184837130/auri_wombo_nobg.png",
+    "https://media.discordapp.net/attachments/765332529812078612/986487099370913842/cosmic_phoenix.jpg",
+    "https://cdn.discordapp.com/attachments/753391562062299149/986518823916167198/20220409_164510.jpg",
+    "https://cdn.discordapp.com/attachments/933032744282357830/933052857568067644/SPOILER_phoenix_1.jpg",
+    "https://i.imgur.com/J15OQ0J.jpg",
+    "https://cdn.discordapp.com/attachments/753391562062299149/913214629277224970/Egg.png",
+  ],
+  links: {
+    patreon: "https://www.patreon.com/SelkieMyth",
+    royalroad: "https://www.royalroad.com/profile/174291",
+    discord: "https://discord.gg/QNFpAk3p",
+  },
+  background:
+    "https://cdn.glitch.global/af094f8f-cb9e-4e46-aef7-ee8b98c8965b/final_banner.jpg?v=1657176560875",
+};
+
+// onload
+window.onload = function () {
+  // set up the author
+  const author = document.getElementById("author");
+  author.innerHTML = data.author.name;
+
+  // set up the bio
+  const bio = document.getElementById("bio");
+  bio.innerHTML = data.author.bio;
+
+  // set up the description
+  const description = document.getElementById("description");
+  description.innerHTML = data.author.description;
+
+  // set up the pen name
+  const penName = document.getElementsByClassName("penName");
+  for (let i = 0; i < penName.length; i++) {
+    penName[i].innerHTML = data.author.penName;
+  }
+
+  // set up the image
+  const image = document.getElementsByClassName("profileicon");
+  for (let i = 0; i < image.length; i++) {
+    image[i].src = data.author.image;
+    image[i].href = data.author.image;
+  }
+
+  // set up the books
+  const books = document.getElementById("bookholder");
+  for (let i = 0; i < data.books.length; i++) {
+    const book = document.createElement("a");
+    book.href = data.books[i].link;
+    book.target = "_blank";
+    book.innerHTML = `<img src="${data.books[i].image}">`;
+    books.appendChild(book);
+  }
+
+  // set up the artwork
+  //style="width: 100%;border-radius:50px"
+  for (let i = 0; i < data.artwork.length; i++) {
+    const art = document.createElement("img");
+    art.src = data.artwork[i];
+    art.style = "width: 100%;border-radius:50px";
+    const artwork =
+      i < data.artwork.length / 2
+        ? document.getElementById("images-1")
+        : document.getElementById("images-2");
+    artwork.appendChild(art);
+  }
+
+  // set up the links
+  for (let key in data.links) {
+    const link = document.getElementsByClassName(key);
+    for (let i = 0; i < link.length; i++) {
+      link[i].href = data.links[key];
+    }
+  }
+
+  // set up the background
+
+  const background = document.getElementById("background");
+  background.src = data.background;
+};
